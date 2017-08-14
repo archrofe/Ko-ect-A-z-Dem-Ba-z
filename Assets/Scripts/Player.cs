e6d82700b4251adf88fd995dafb8ab9cb58e4c2a
archrofe <30814733+archrofe@users.noreply.github.com>
2017-08-14 14:27:17 +1000

    Add files via upload

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movementSpeed = 5;
+    public float acceleration = 50f;
+    public float deceleration = .1f;
+
+    private Rigidbody2D rigid;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If user presses D
+        if (Input.GetKey(KeyCode.D) || (Input.GetKey(KeyCode.RightArrow)))
+        {
+            // Move right
+            rigid.AddForce(transform.right * acceleration);
+        }
+        //If user presses A
+        if (Input.GetKey(KeyCode.A) || (Input.GetKey(KeyCode.LeftArrow)))
+        {
+            // Move left
+            rigid.AddForce(-transform.right * acceleration);
+        }
+
+        //Deceleration
+        rigid.velocity += -rigid.velocity * deceleration;
+    }
+}
