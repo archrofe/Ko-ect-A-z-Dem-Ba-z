849f1cb47257eff5572cb417d01985de6940cc9e
Duane Reymond <duane.reymond@gmail.com>
2017-08-16 11:49:33 +1000

    16/8 Fix: Added Redball prefab to BallSpawner correctly.

--- Assets/Scripts/BallSpawner.cs
+++ Assets/Scripts/BallSpawner.cs
@@ -10,10 +10,7 @@ namespace LoopsArrays
         public GameObject[] spawnPrefabs;
         public float spawnRadius = 1f;
         public int spawnAmount = 5;
-        // public float frequency = 5;
-        // public float amplitude = 6;
-
-        
+                
 
         void OnDrawGizmos()
         {
@@ -25,9 +22,7 @@ namespace LoopsArrays
         {
 
             InvokeRepeating("SpawnBalls", 2.0f, 1.0f);
-            // InvokeRepeating([function], [start seconds float], [repeating seconds float])
-
-
+            
         }
 
         // Update is called once per frame
@@ -38,7 +33,7 @@ namespace LoopsArrays
 
 
 
-        void SpawnBalls() // for 3D
+        void SpawnBalls()
         {
             for (int i = 0; i < spawnAmount; i++)
             {
@@ -56,10 +51,6 @@ namespace LoopsArrays
                 float y = 0;
                 float z = 0; // 
                 Vector3 randomPos = transform.position + new Vector3(x, y, z); // calculate random position
-
-                // Cancel out the Z
-                // randomPos.z = 0;
-                // randomPos.y = 0;
 
                 // Set spawned object's position
                 clone.transform.position = randomPos;                               
