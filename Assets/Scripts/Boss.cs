fdb3ca11ff15720a9ed44210b2f0e6553e0cb6a8
Duane Reymond <duane.reymond@gmail.com>
2017-09-14 20:45:18 +1000

    14/9: -Added more Bosswork. Need to fix sideways movement.

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class Boss : MonoBehaviour
 {
     public Transform pos1; // down!
+    public Transform pos2; // right!
+    public Transform pos3; // left!
     public float pos1Speed;
 
     public bool bossStarted = false;
+    public bool bossFirstMove = false;
+
+    public GameObject bossSpawner1;
+    public GameObject bossSpawner2;
 
 	// Use this for initialization
 	void Start ()
@@ -18,8 +24,41 @@ public class Boss : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        GoToPos1();
-        
+        if (bossStarted == false)
+        {
+
+            GoToPos1();
+
+            if (transform.position == pos1.position)
+            {
+                bossSpawner1.SetActive(true);
+                bossSpawner2.SetActive(true);
+                bossStarted = true;
+            }
+        }
+
+        if (bossStarted == true)
+        {
+            if (bossFirstMove == false)
+            {
+                GoRight();
+                bossFirstMove = true;
+            }
+
+            if (bossFirstMove == true)
+            {
+                if (transform.position == pos2.position)
+                {
+                    GoLeft();
+                }
+
+                if (transform.position == pos3.position)
+                {
+                    GoRight();
+                }
+            }
+        }
+
         /*if (bossStarted == false)
         {
             GoToPos1();
@@ -41,5 +80,13 @@ public class Boss : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, pos1.position, pos1Speed * Time.deltaTime);
     }
 
+    void GoRight()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, pos2.position, pos1Speed * Time.deltaTime);
+    }
 
+    void GoLeft()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, pos3.position, pos1Speed * Time.deltaTime);
+    }
 }
