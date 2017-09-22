ad30b779ed578293f2e4caf53d37afa9c18687d7
Duane Reymond <duane.reymond@gmail.com>
2017-09-22 20:06:18 +1000

    More tweaks to Win and Loss Screens (backgrounds!).

---
+++ Assets/Scripts/BirdsWin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdsWin : MonoBehaviour
+{
+    public Transform birdEndPos;
+    public float birdSpeed;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float step = birdSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, birdEndPos.position, step);
+    }
+}
