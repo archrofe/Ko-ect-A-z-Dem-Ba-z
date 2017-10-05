f820167ec034197b10b2d6bc2c782e7dab7285d0
Duane Reymond <duane.reymond@gmail.com>
2017-10-06 10:35:11 +1100

    -BossSpawner script added to Boss Scene. -Original MIDI music added to Menu.

---
+++ Assets/Scripts/BossSpawner.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LoopsArrays
+{
+    public class BossSpawner : MonoBehaviour
+    {
+        public GameObject[] spawnPrefabs;
+        public float spawnRadius = 1f;
+        public int spawnAmount = 1;
+        public int currentSpawn;
+        public int maxSpawn = 10;
+        public float spawnInterval = 1f;
+
+        private bool hasBalled = false;
+
+        void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(transform.position, spawnRadius);
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            currentSpawn = 0;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!hasBalled && currentSpawn < maxSpawn)
+            {
+
+                StartCoroutine(Fire());
+                currentSpawn++;
+            }
+        }
+
+        IEnumerator Fire()
+        {
+            // run whatever is here first
+            hasBalled = true;
+
+            // Spawn the bullet
+            SpawnBalls();
+
+            yield return new WaitForSeconds(spawnInterval); // wait a few seconds
+
+            // run whatever is here last
+            hasBalled = false;
+        }
+
+        void SpawnBalls()
+        {
+            for (int i = 0; i < spawnAmount; i++)
+            {
+                // Spawned new GameObject
+                int randomIndex = Random.Range(0, spawnPrefabs.Length);
+
+                // Store randomly selected prefab
+                GameObject randomPrefab = spawnPrefabs[randomIndex];
+
+                // Spawned new GameObject
+                GameObject clone = Instantiate(randomPrefab);
+
+                // Calculate random position within sphere
+                Vector3 randomPos = transform.position + Random.insideUnitSphere * spawnRadius; // calculate random position
+
+                // Cancel out the Z
+                randomPos.z = 0;
+
+                // Set spawned object's position
+                clone.transform.position = randomPos;
+            }
+        }
+    }
+}
